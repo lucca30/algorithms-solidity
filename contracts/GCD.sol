fd41fb0cbf07fe6022e511a0331e6527f9f9f441
Lucca Martins <lucca_martins30@yahoo.com.br>
2022-10-01 11:47:02 -0300

    Improvig encludian algorithm

--- contracts/GCD.sol
+++ contracts/GCD.sol
@@ -1,12 +1,17 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract GCD {
-    // function that only contract owner can run, to set a new message
     function euclidianAlgorithm(int256 a, int256 b) public returns (int256) {
         require(a >= b && b >= 0 && a > 0);
-        // set new message
-        if (b == 0) return a;
-        return euclidianAlgorithm(b, a % b);
+        int256 _a = a;
+        int256 _b = b;
+        int256 temp;
+        while (_b > 0) {
+            temp = _b;
+            _b = _a % _b; 
+            _a = temp;
+        }
+        return _a;
     }
 
     function verifyGcd(int256 a, int256 b, int256 gcd, int256 x, int256 y) public returns (int256) {
