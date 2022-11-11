ea6d26d9254f5b3a8821c8f2f6d8e713d5feae9f
Lucca Martins <lucca_martins30@yahoo.com.br>
2022-11-11 16:11:13 -0300

    melhorando geracao de resultados

--- test/GCD_data_generation.js
+++ test/GCD_data_generation.js
@@ -1,23 +1,24 @@
 const web3 = require("web3");
 const fs = require('fs');
+const BN = require('bn.js');
 
 var randomnumber = (maximum, minimum) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 var randomBN = () => {
-  const value = web3.utils.randomHex(randomnumber(10, 1))
+  const value = web3.utils.randomHex(30)
   const bn = web3.utils.toBN(value.toString('hex'), 16);
-  return bn;
+  return new BN(bn.toString().substring(0, randomnumber(1, 30))); // gera um número aleatório uniformemente distribuido em relacao ao numero de caracteres
 }
 
 const resp = [];
 const x = [];
-for(let i = 0;i<300 ;i+=1){
+for(let i = 0;i<1000 ;i+=1){
   const a = randomBN();
   const b = randomBN();
   if(a.gt(b))resp.push({a:a.toString(),b:b.toString()});
   else resp.push({a:b.toString(),b:a.toString()});
 }
 
-fs.writeFile('test/GCD_data.json', JSON.stringify(resp), err => {
+fs.writeFile('test/GCD_data.json', JSON.stringify(resp, null, 4), err => {
   if (err) {
     console.error(err);
   }
